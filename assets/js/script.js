040c9b332949c965d2e0ea82535041eb6d71fe4e
Alexis DEGRYSE <alexis.degryse@gmail.com>
2022-06-30 12:06:58 +0200

    Some neon stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,43 @@
-function initText() {
+function generateText(paragraphes_number) {
+
+	// Sentences version
+	document.querySelectorAll('.sentences p').forEach(function(p) {
+		if([...p.parentElement.children].indexOf(p)	< paragraphes_number) {
+			p.style.display = '';
+		} else {
+			p.style.display = 'none';
+		}
+	});
+
+	// Words version
+	document.querySelectorAll('.words p:not(:first-of-type)').forEach(function(p) {
+		p.parentNode.removeChild(p);
+	});
+	let firstWordsBlock = document.querySelector('.words p');
+	for (let i = 1; i < paragraphes_number; i++) {
+		let clone = firstWordsBlock.cloneNode(true);
+		firstWordsBlock.after(clone);
+	}
+
+	// Fill terms
 	document.querySelectorAll('[data-term]').forEach(function(elmt) {
 		let type = terms[elmt.getAttribute('data-term')];
 		elmt.textContent = type[Math.floor(Math.random() * type.length)];
 	});
+
+	// Fill dates
 	document.querySelectorAll('[data-date]').forEach(function(elmt) {
 		elmt.textContent = new Date().getFullYear() - Math.floor(Math.random() * 35);
 	});
+
+	// Fill notes
 	document.querySelectorAll('[data-note]').forEach(function(elmt) {
 		elmt.textContent = Math.floor(Math.random() * 75) + '/' + Math.floor(Math.random() * 26 + 75);
 	});
 }
 
-initText();
+generateText(document.getElementById('paragraphes-number').value);
 
 document.getElementById('generate').addEventListener('click', () => {
-	initText();
+	generateText(document.getElementById('paragraphes-number').value);
 });
--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,25 @@
 module.exports = function (grunt) {
+	const sass = require('sass');
 
 	/**
    * [Just-in-time plugin loader: improve speed tasks]
    */
-	require('jit-grunt')(grunt, {});
+	require('jit-grunt')(grunt, {
+		htmllint: 'grunt-html'
+	});
 
 	/**
    * [Paths]
    */
 	const jsPath = 'assets/js/';
+	const cssPath = 'assets/css/';
+
+	/**
+   * [PostCSS Plugins]
+   */
+	const autoprefixer = require('autoprefixer');
+	const cssnano = require('cssnano');
+	const mqsort = require('node-css-mqpacker');
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
@@ -18,6 +29,7 @@ module.exports = function (grunt) {
 		browserSync: {
 			bsFiles: {
 				src: [
+					`${cssPath}**/*.css`,
 					`${jsPath}**/*.js`,
 					`index.html`,
 				],
@@ -25,6 +37,103 @@ module.exports = function (grunt) {
 			options: {
 				watchTask: true,
 				proxy: 'http://localhost/videorem-gamipsum/',
+			},
+		},
+		/**
+     * [Minify HTML]
+     */
+		htmlmin: {
+			options: {
+				removeComments: true,
+				collapseWhitespace: true,
+				collapseInlineTagWhitespace: false,
+			},
+			dist: {
+				files: {
+		      'index.html': 'index.unmin.html'
+		    }
+			}
+		},
+		/**
+     * [HTML Validation]
+     */
+    htmllint: {
+		  options: {
+		  	noLangDetect: true,
+		  	force: true,
+		    errorlevels: ['error']
+		  },
+    	all: [ 'index.unmin.html' ]
+    },
+		/**
+	   * [Sass compiler]
+	   */
+		sass: {
+			options: {
+				implementation: sass,
+				sourceMap: true,
+				sourceComments: true,
+				outputStyle: 'expanded',
+			},
+			dist: {
+				files: [ {
+					expand: true,
+					src: [ '**/*.scss' ],
+					cwd: cssPath,
+					dest: cssPath,
+					ext: '.css',
+				} ],
+			},
+		},
+		shell: {
+			options: {
+				failOnError: false,
+			},
+			/**
+       * [Stylelint]
+       */
+			lint: {
+				command: `stylelint "${cssPath}**/*.scss" --config ${cssPath}.stylelintrc --fix`,
+			},
+		},
+		/**
+     * [Apply several post-processors to CSS using PostCSS]
+     */
+		postcss: {
+			dist: {
+				options: {
+					map: {
+						inline: false, // save all sourcemaps as separate files...
+						annotation: `${cssPath}/sourcemaps/`, // ...to the specified directory
+					},
+					processors: [
+						/**
+	           * [Adds vendor prefixes, using data from Can I Use]
+	           */
+						autoprefixer({
+							overrideBrowserslist : [ 'last 2 Chrome versions', 'last 2 Firefox versions', 'last 2 Edge versions', 'last 2 Safari versions', 'last 2 iOS versions', 'last 2 ChromeAndroid versions' ],
+						}),
+						/**
+		         * [Pack same CSS media query rules]
+		         */
+						mqsort({
+	          	sort: true
+	          }),
+						/**
+	           * [Optimize CSS size]
+	           */
+						cssnano({
+							safe: true,
+						}),
+					],
+				},
+				files: [ {
+					expand: true,
+					cwd: cssPath,
+					src: [ 'styles.css' ],
+					dest: cssPath,
+					ext: '.min.css',
+				} ],
 			},
 		},
 		/**
@@ -74,6 +183,14 @@ module.exports = function (grunt) {
 			options: {
 				spawn: false,
 			},
+			html: {
+				files: '**/*.html',
+				tasks: [ 'htmllint', 'htmlmin' ],
+			},
+			style: {
+				files: [ `${cssPath}**/*.scss` ],
+				tasks: [ 'sass', 'postcss' ],
+			},
 			js: {
 				files: [ `${jsPath}**/*.js` ],
 				tasks: [ 'jshint', 'babel', 'uglify' ],
@@ -83,5 +200,13 @@ module.exports = function (grunt) {
 
 	grunt.registerTask('default', [ 'browserSync', 'watch' ]);
 
-	grunt.registerTask('build', [ 'jshint', 'babel', 'uglify' ]);
+	grunt.registerTask('build:html', [ 'htmllint', 'htmlmin' ]);
+
+	grunt.registerTask('lint:css', [ 'shell:lint' ]);
+
+	grunt.registerTask('build:css', [ 'sass', 'postcss', 'shell:lint' ]);
+
+	grunt.registerTask('build:js', [ 'jshint', 'babel', 'uglify' ]);
+
+	grunt.registerTask('build', [ 'build:html', 'build:css', 'build:js' ]);
 };
